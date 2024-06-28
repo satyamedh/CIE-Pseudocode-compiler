a9135b3357d6966949efaeea2193452629ffcd70
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2024-06-28 20:53:49 +0530

    Moved the compiling part outside the loop in tests.py
    I'm retarded

--- tests.py
+++ tests.py
@@ -10,17 +10,17 @@ def test_program(file_name):
     # the solutions are stored in the trials variable
     # each trial is a list of inputs and expected outputs
 
-    for index, trial in enumerate(solutions['trials']):
-        # run the main.py file with the input
-        # os.system(f'python main.py tests/{file_name}.psc -o temp/temp -c temp/temp.c')
-        compiler_process = subprocess.Popen(['python', 'main.py', f'tests/{file_name}.psc', '-o', 'temp/temp', '-c',
-                                             'temp/temp.c'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        stdout, stderr = compiler_process.communicate()
-        if stderr:
-            print(f"Compilation failed for {file_name}")
-            print(stderr)
-            return
+    # run the main.py file with the input
+    # os.system(f'python main.py tests/{file_name}.psc -o temp/temp -c temp/temp.c')
+    compiler_process = subprocess.Popen(['python', 'main.py', f'tests/{file_name}.psc', '-o', 'temp/temp', '-c',
+                                         'temp/temp.c'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    stdout, stderr = compiler_process.communicate()
+    if stderr:
+        print(f"Compilation failed for {file_name}")
+        print(stderr)
+        return
 
+    for index, trial in enumerate(solutions['trials']):
         input_data = str(trial[0])  # Goes to STDIN
         expected_output = str(trial[1])  # Comes from STDOUT
 
---
+++ tests/odd_even.psc
@@ -0,0 +1,7 @@
+DECLARE x: INTEGER
+INPUT x
+IF x MOD 2 = 0 THEN
+    PRINT "Even"
+ELSE
+    PRINT "Odd"
+ENDIF
